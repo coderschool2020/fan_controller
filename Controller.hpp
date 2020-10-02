1867fe53735cf45c65163c86332ab2f7d0c86ddc
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2020-10-02 16:08:07 +0100

    Add private members & constructor parameters

--- Controller.hpp
+++ Controller.hpp
@@ -1,13 +1,26 @@
 #pragma once
 #include "SlowThermometer/SlowThermometer.hpp"
 #include "Fan.hpp"
+#include "LcdDisplay.hpp"
 #include <memory>
 
 class LcdDisplay;
 
 class Controller {
 public:
-    Controller(SlowThermometer, Fan, double, double, std::shared_ptr<LcdDisplay>);
+    Controller(SlowThermometer thermometer,
+               Fan fan,
+               double targetTemperature,
+               double tolerance,
+               std::shared_ptr<LcdDisplay> lcd);
     void updateRpm();
     void displayInfo();
+
+private:
+    SlowThermometer& thermometer_;
+    Fan& fan_;
+    double targetTemperature_;
+    double tolerance_;
+    std::shared_ptr<LcdDisplay> lcd_;
+    static double currentTemperature_;
 };
