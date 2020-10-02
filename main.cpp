3de22021f18f7f89d74a02e70b43c072ffcf007c
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2020-10-02 16:03:07 +0100

    Add instance of lcd display

--- main.cpp
+++ main.cpp
@@ -5,23 +5,30 @@
 #include <memory>
 #include <iostream>
 
-int main()
-try {
+int main() try {
     SlowThermometer t;
     Fan f;
     double targetTemperature = 36.6;
     double tolerance = .5;
+    auto lcdDisplay = std::make_shared<LcdDisplay>();
+    
+    // no display
     Controller oldController{t, f, targetTemperature, tolerance, nullptr};
     oldController.updateRpm();
     oldController.displayInfo();
 
+    // no display
     Controller newController{t, f, targetTemperature, tolerance, std::shared_ptr<LcdDisplay>()};
     newController.updateRpm();
     newController.displayInfo();
+
+    // instance of display
+    Controller newestController{t, f, targetTemperature, tolerance, lcdDisplay};
+    newestController.updateRpm();
+    newestController.displayInfo();
 
     return 0;
 } catch (const std::exception & e) {
     std::cerr << e.what() << '\n';
     return -1;
 }
-
