9cf1156334043313b8b5102a2639118dc0c3a9f6
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2020-10-14 12:59:34 +0100

    Remove constructors (apply rule of 0)

--- Fan.cpp
+++ Fan.cpp
@@ -2,14 +2,6 @@
 #include <stdexcept>
 #include <cmath>
 
-Fan::Fan() {
-    rpm = 0;
-}
-
-Fan::Fan(Fan&& other) : rpm(std::move(other.rpm)) {}
-
-Fan::Fan(const Fan& other) : rpm(other.rpm) {}
-
 void Fan::setSpeed(int newRpm) {
     if ((newRpm < 1000 and newRpm != 0) or newRpm > 3000) {
         throw std::invalid_argument("Invalid speed");
--- Fan.hpp
+++ Fan.hpp
@@ -4,10 +4,6 @@ class Fan {
     int rpm = 0;
 
 public:
-    Fan();
-    Fan(const Fan&);
-    Fan(Fan&&);
-
     void setSpeed(int newRpm);
     int getSpeed();
 };
