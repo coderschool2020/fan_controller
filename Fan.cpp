d3203e4792a4de8d2f83d4bab57a78bddf2224c1
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2020-10-14 12:56:30 +0100

    Simplify setSpeed() method

--- Fan.cpp
+++ Fan.cpp
@@ -28,14 +28,7 @@ void Fan::setSpeed(int newRpm) {
     if ((newRpm < 1000 and newRpm != 0) or newRpm > 3000) {
         throw std::invalid_argument("Invalid speed");
     }
-    auto difference = std::abs(newRpm - rpm);
-    for (auto i = 0; i < difference; ++i) {
-        if (newRpm - rpm > 0) {
-            rpm++;
-        } else {
-            rpm--;
-        }
-    }
+    rpm = newRpm;
 }
 
 int Fan::getSpeed() const {
